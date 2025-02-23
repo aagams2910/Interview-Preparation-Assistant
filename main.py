c2587402d2d17b09fa1c9e43571f138917634282
Aagam Shah <aagams2910@gmail.com>
2025-02-23 23:58:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-# Disable oneDNN log messages by settenvironm variable before any TensorFlow or related imports
+# Disable oneDNN log messages by settenvironm variable before any TensorFlow or related import
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 import streamlit as st
